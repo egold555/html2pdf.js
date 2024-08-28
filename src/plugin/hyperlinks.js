a05e76b603dda15be014ab3b1df37fc21abe8174
Eric Golde <eric@golde.org>
2024-08-28 10:57:27 -0700

    Beautify

--- src/plugin/hyperlinks.js
+++ src/plugin/hyperlinks.js
@@ -1,5 +1,5 @@
-import Worker from '../worker.js';
-import { unitConvert } from '../utils.js';
+import Worker from "../worker.js";
+import { unitConvert } from "../utils.js";
 
 // Add hyperlink functionality to the PDF creation.
 
@@ -16,26 +16,36 @@ Worker.prototype.toContainer = function toContainer() {
     if (this.opt.enableLinks) {
       // Find all anchor tags and get the container's bounds for reference.
       var container = this.prop.container;
-      var links = container.querySelectorAll('a');
-      var containerRect = unitConvert(container.getBoundingClientRect(), this.prop.pageSize.k);
+      var links = container.querySelectorAll("a");
+      var containerRect = unitConvert(
+        container.getBoundingClientRect(),
+        this.prop.pageSize.k
+      );
       linkInfo = [];
 
       // Loop through each anchor tag.
-      Array.prototype.forEach.call(links, function(link) {
-        // Treat each client rect as a separate link (for text-wrapping).
-        var clientRects = link.getClientRects();
-        for (var i=0; i<clientRects.length; i++) {
-          var clientRect = unitConvert(clientRects[i], this.prop.pageSize.k);
-          clientRect.left -= containerRect.left;
-          clientRect.top -= containerRect.top;
+      Array.prototype.forEach.call(
+        links,
+        function (link) {
+          // Treat each client rect as a separate link (for text-wrapping).
+          var clientRects = link.getClientRects();
+          for (var i = 0; i < clientRects.length; i++) {
+            var clientRect = unitConvert(clientRects[i], this.prop.pageSize.k);
+            clientRect.left -= containerRect.left;
+            clientRect.top -= containerRect.top;
 
-          var page = Math.floor(clientRect.top / this.prop.pageSize.inner.height) + 1;
-          var top = this.opt.margin[0] + clientRect.top % this.prop.pageSize.inner.height;
-          var left = this.opt.margin[1] + clientRect.left;
+            var page =
+              Math.floor(clientRect.top / this.prop.pageSize.inner.height) + 1;
+            var top =
+              this.opt.margin[0] +
+              (clientRect.top % this.prop.pageSize.inner.height);
+            var left = this.opt.margin[1] + clientRect.left;
 
-          linkInfo.push({ page, top, left, clientRect, link });
-        }
-      }, this);
+            linkInfo.push({ page, top, left, clientRect, link });
+          }
+        },
+        this
+      );
     }
   });
 };
@@ -45,10 +55,15 @@ Worker.prototype.toPdf = function toPdf() {
     // Add hyperlinks if the option is enabled.
     if (this.opt.enableLinks) {
       // Attach each anchor tag based on info from toContainer().
-      linkInfo.forEach(function(l) {
+      linkInfo.forEach(function (l) {
         this.prop.pdf.setPage(l.page);
-        this.prop.pdf.link(l.left, l.top, l.clientRect.width, l.clientRect.height,
-                           { url: l.link.href });
+        this.prop.pdf.link(
+          l.left,
+          l.top,
+          l.clientRect.width,
+          l.clientRect.height,
+          { url: l.link.href }
+        );
       }, this);
 
       // Reset the active page of the PDF to the final page.
